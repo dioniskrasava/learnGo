8d97bee08f4babbae63cd152e2c1326ab68dd189
Ivan Belov <dionis.krasava@gmail.com>
2025-02-25 18:41:37 +0700

    6 база - побитовые операции

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import base "learnExample/conspect"
 
 func main() {
 	// https://metanit.com/go/tutorial/
-	base.ExampleVar()             // 2.2 Переменные
-	base.ExampleTypes()           // 2.3 Типы данных
-	base.ExampleConst()           // 2.4 Константы
-	base.ExampleArithOperations() // 2.5 Арифметические операции
+	base.ExampleVar()                    // 2.2 Переменные
+	base.ExampleTypes()                  // 2.3 Типы данных
+	base.ExampleConst()                  // 2.4 Константы
+	base.ExampleArithOperations()        // 2.5 Арифметические операции
+	base.ExampleConditionalExpressions() // 2.6 Условные выражения
+	base.ExampleBitwiseOperations()      // 2.7 Поразрядные операции
+	base.ExampleArrays()                 //2.8 Массивы
 }
