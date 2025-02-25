53919206240952069d55febe2ca0442a8a5c3168
Ivan Belov <dionis.krasava@gmail.com>
2025-02-25 14:52:59 +0700

    +

---
+++ go.mod
@@ -0,0 +1,3 @@
+module learnExample
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import base "learnExample/conspect"
+
+func main() {
+	// https://metanit.com/go/tutorial/
+	base.ExampleVar()             // 2.2 Переменные
+	base.ExampleTypes()           // 2.3 Типы данных
+	base.ExampleConst()           // 2.4 Константы
+	base.ExampleArithOperations() // 2.5 Арифметические операции
+}
